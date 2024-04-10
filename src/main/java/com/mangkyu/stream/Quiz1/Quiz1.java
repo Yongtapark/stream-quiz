af4a131154206b8c6ea7d8c3ca8a1a6c1dc08710
choc0villain <sunww2017@naver.com>
2024-04-11 01:08:36 +0900

    문제풀이

--- src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
+++ src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -1,30 +1,55 @@
 package com.mangkyu.stream.Quiz1;
 
 import com.opencsv.CSVReader;
+import com.sun.org.apache.bcel.internal.generic.ARETURN;
+import com.sun.org.apache.bcel.internal.generic.DRETURN;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Quiz1 {
 
     // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz1() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        return csvLines.stream()
+                .map(line -> line[1].trim())
+                .flatMap(h -> Arrays.stream(h.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
     }
 
     // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
     public Map<String, Integer> quiz2() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return new HashMap<>();
+
+        return csvLines.stream()
+                .filter(line -> line[0].contains("정"))
+                .map(line -> line[1].trim())
+                .flatMap(h -> Arrays.stream(h.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> newValue += oldValue));
+
     }
 
     // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
     public int quiz3() throws IOException {
         List<String[]> csvLines = readCsvLines();
-        return 0;
+        int cnt = 0;
+        String collect = csvLines.stream()
+                .map(line -> line[2])
+                .filter(str -> str.contains("좋아"))
+                .collect(Collectors.joining());
+
+        for (int i = 0; i < collect.length(); i++) {
+            if (collect.startsWith("좋아", i)) {
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 
     private List<String[]> readCsvLines() throws IOException {
